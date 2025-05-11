a8ce2e2d4331b6679604d543bed3386a8230127d
James Tuttle <james.j.tuttle@gmail.com>
2025-05-11 06:29:04 -0700

    save work to tesr action.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,5 +4,5 @@ import json
 def lambda_function(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello updated Lambda from cursor')
+        'body': json.dumps('Hello updated Lambda from cursor!')
     }
